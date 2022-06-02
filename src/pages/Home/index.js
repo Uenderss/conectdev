6c3a0335a7f0e93cf0a44008c65640660a38dd65
Uenderss <meajudabarto@gmail.com>
2022-06-01 21:41:13 -0300

    Initial Code

---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import './style.css';
+
+function Home(){
+    return <>
+       <Header/>
+        <main className=''>
+            <nav className='navbar'>
+
+            </nav>
+            <section className='feed'></section>
+        </main>
+    </>
+}
+
+export default Home;
